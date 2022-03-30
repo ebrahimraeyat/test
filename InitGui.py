2a8343a3054f2afd8ac4c446f84870efe30a5e4f
ebrahimraeyat <ebe79442114@yahoo.com>
2022-03-30 12:41:08 +0430

    correct initialize

--- InitGui.py
+++ InitGui.py
@@ -4,6 +4,16 @@ class SubWorkbench(Workbench):
 
     def __init__(self):
 
-        pass
+        from pathlib import Path
+        import welcom
+        self.__class__.Icon = str(Path(welcom.__file__).parent.absolute()  / 'sub.svg')
+        self.__class__.MenuText = "SSSSSSS"
+        self.__class__.ToolTip = "Sub Workbench"
+
+    def Initialize(self):
+        print('ssssss started')
+
+    def Activated(self):
+        print('sssss activated')
 
 Gui.addWorkbench(SubWorkbench())
